53d2ca44d0f27207a06fddb63c0fe1f801612db0
salinoo7 <lucassalino3008@outlook.com>
2023-03-06 15:23:11 -0300

    Repositorio destinado a praticas com c#

---
+++ EConceito.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PraticaCsharp
+{
+    public enum EConceito 
+    {
+        A,
+        B,
+        C,
+        D,
+        E,
+    }
+}
